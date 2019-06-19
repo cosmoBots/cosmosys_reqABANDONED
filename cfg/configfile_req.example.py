006e7798caeed04aa030e7d4b4c8e4cf33063e32
Txinto Vaz <txinto@iac.es>
2019-06-19 16:19:00 +0100

    Upload feature does not depend on literal values anymore.

--- cfg/configfile_req.example.py
+++ cfg/configfile_req.example.py
@@ -1,27 +1,51 @@
 # This section configures the connection between the CosmoSys_Req tools and the Redmine instance which implements the requirements database
 req_server_url = 'http://myrequirementsserver.domain'	# The Redmine URL
-req_key_txt = 'the bot API key of the redmine'			# The API key of the user (or bot) in which name the actions are undertaken.
+req_key_txt = 'the API key of the redmine'			# The API key of the user (or bot) in which name the actions are undertaken.
 req_project_id_str = 'myprojectid'						# The ID of the Redmine project where the requirements are stored.
 req_rq_tracker_id = 11									# The numerical ID of the tracker used to model the requirements
-req_doc_tracker_id = 16									# The numerical ID of the tracker used to model the documents of requirements
+req_doc_tracker_id = 19									# The numerical ID of the tracker used to model the documents of requirements
 req_relation_str = 'blocks'								# The redmine relationship type used to map the requirements dependence relationships
 # Custom field mapping of the requirement attributes
 # Note: the ID is mapped on Redmine issue ID field, the RqID is mapped on Subject field, the RqTarget is mapped on Version field.
 # Note: the RqDoc is mapped using the Parent relationship between the requirement (child) and the document (father).  The document is using a different tracker (RqDoc) from the requirement (Req)
 req_title_cf_id = 42									# The numerical ID of the custom_field used to store the title of the requirement
-req_source_cf_id = 10									# The numerical ID of the custom_field used to store the RqSources of the requirement
+req_sources_cf_id = 10									# The numerical ID of the custom_field used to store the RqSources of the requirement
 req_type_cf_id = 8										# The numerical ID of the custom_field used to store the RqType of the requirement
 req_level_cf_id = 11									# The numerical ID of the custom_field used to store the RqLevel of the requirement
 req_rationale_cf_id = 12								# The numerical ID of the custom_field used to store the RqRationale of the requirement
-req_value_id = 44										# The numerical ID of the custom_field used to store the RqValue of the requirement
-req_var_id = 44											# The numerical ID of the custom_field used to store the RqVar of the requirement
-req_chapter_id = 44 									# The numerical ID of the custom_field used to store the RqChapter of the requirement
+req_value_cf_id = 44									# The numerical ID of the custom_field used to store the RqValue of the requirement
+req_var_cf_id = 45										# The numerical ID of the custom_field used to store the RqVar of the requirement
+req_chapter_cf_id = 43 									# The numerical ID of the custom_field used to store the RqChapter of the requirement
+req_prefix_cf_id = 46									# The numerical ID of the custom_field used to store the requirements document prefix of the requirement
 
 # This section defines the connection between the CosmoSys_Req tools and the OpenDocument spreadsheet used for importing requirements
 req_upload_file_name = "RqUpload.ods"					
 req_upload_start_column = 0
-req_upload_end_column = 6
+req_upload_end_column = 15
 req_upload_start_row = 0
-req_upload_end_row = 200
+req_upload_end_row = 199
+
+#This section defines the document information cell indexes to retrieve information for the documents from the upload file
+req_upload_doc_row = 0
+req_upload_doc_name_column = 1
+req_upload_doc_prefix_column = 5
+req_upload_doc_parent_column = 3
+
+#This section defines the requirements information cell indexes to retrieve information for the requirements from the upload file
+req_upload_first_row = 2
+req_upload_column_number = 16
+req_upload_id_column = 4
+req_upload_related_column = 10
+req_upload_title_column = 5
+req_upload_descr_column = 6
+req_upload_source_column = 11
+req_upload_type_column = 9
+req_upload_level_column = 8
+req_upload_rationale_column = 15
+req_upload_var_column = 13
+req_upload_value_column = 14
+req_upload_chapter_column = 0
+req_upload_target_column = 12
+req_upload_parent_column = 7
 
 
